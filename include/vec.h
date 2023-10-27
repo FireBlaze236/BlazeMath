ddd9a63642cb5e4707a75b8395e63b17d9cdbbc1
FireBlaze236 <taufiqul@vigaet.com>
2023-10-28 00:20:42 +0600

    Vec const functions update

--- include/vec.h
+++ include/vec.h
@@ -22,8 +22,8 @@ namespace BlazeMath {
 		inline Vec2D(float a) : e{ a, a } {}
 		inline Vec2D(float a, float b) : e{ a, b } {}
 
-		inline float length_squared() { return x * x + y * y; }
-		inline float length() { return std::sqrt(length_squared()); }
+		inline float length_squared() const { return x * x + y * y; }
+		inline float length() const { return std::sqrt(length_squared()); }
 
 		//Operator overloads
 		Vec2D& operator=(const Vec2D& rhs);
@@ -35,7 +35,7 @@ namespace BlazeMath {
 		Vec2D& operator*=(const float rhs);
 		Vec2D& operator/=(const float rhs);
 
-		inline Vec2D normalized() { float l = 1.0f / length(); return Vec2D(x * l, y * l); }
+		inline Vec2D normalized() const { float l = 1.0f / length(); return Vec2D(x * l, y * l); }
 		inline Vec2D& normalize() { float l = 1.0f / length(); x *= l;  y *= l; return *this; }
 
 	};
@@ -65,8 +65,8 @@ namespace BlazeMath {
 		inline Vec3D() : e{ 0,0,0 } {}
 		inline Vec3D(float a, float b, float c) : e{ a, b, c } {}
 
-		inline float length_squared() { return x * x + y * y + z * z; }
-		inline float length() { return std::sqrt(length_squared()); }
+		inline float length_squared() const { return x * x + y * y + z * z; }
+		inline float length() const { return std::sqrt(length_squared()); }
 		//Operator overloads
 		Vec3D& operator=(const Vec3D& rhs);
 		Vec3D& operator+=(const Vec3D& rhs);
@@ -77,7 +77,7 @@ namespace BlazeMath {
 		Vec3D& operator*=(const float rhs);
 		Vec3D& operator/=(const float rhs);
 
-		inline Vec3D normalized() { float l = 1.0f / length(); return Vec3D(x * l, y * l, z * l); }
+		inline Vec3D normalized() const { float l = 1.0f / length(); return Vec3D(x * l, y * l, z * l); }
 		inline Vec3D& normalize() { float l = 1.0f / length(); x *= l;  y *= l; z *= l; return *this; }
 
 		inline float dot(Vec3D& other) { return x * other.x + y * other.y + z * other.z; }
@@ -106,9 +106,9 @@ namespace BlazeMath {
 		inline Vec4D(float a) : e{ a, a, a, a } {}
 		inline Vec4D(float a, float b, float c, float d) : e{ a, b, c, d } {}
 
-		inline float length_squared() { return x * x + y * y + z * z + w * w; }
-		inline float length() { return std::sqrt(length_squared()); }
-		inline Vec4D normalized() { float l = 1.0f / length(); return Vec4D(x * l, y * l, z * l, w * l); }
+		inline float length_squared() const { return x * x + y * y + z * z + w * w; }
+		inline float length() const { return std::sqrt(length_squared()); }
+		inline Vec4D normalized() const { float l = 1.0f / length(); return Vec4D(x * l, y * l, z * l, w * l); }
 		inline Vec4D& normalize() { float l = 1.0f / length(); x *= l;  y *= l; z *= l; w *= l; return *this; }
 		inline float dot(Vec4D& other) { return x * other.x + y * other.y + z * other.z + w * other.w; }
 		Vec4D& operator=(const Vec4D& rhs);
